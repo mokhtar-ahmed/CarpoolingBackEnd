d9bf7b912c9bd3e441ed76df45c2b4017f8d1f7b
SarahMohamed268 <sara.mohamed269@gmail.com>
2014-05-18 07:16:04 -0700

    Integration V1.2

--- CarpoolingBackendFinal/src/java/dao/JoinEventDAO.java
+++ CarpoolingBackendFinal/src/java/dao/JoinEventDAO.java
@@ -2,10 +2,15 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ *//*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 
 package dao;
 
+import java.sql.SQLException;
 import java.util.List;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
@@ -17,18 +22,31 @@ import pojo.*;
  * @author Nourhan
  */
 public class JoinEventDAO {
-    static Session session;
+    
+    private static Session session;
+    
+    //private static JoinEventDAO joinEventDAO ; 
+    
     public JoinEventDAO()
     {
+        
         session = HibernateUtil.getSessionFactory().openSession();
     }
     
+//    
+//    public static JoinEventDAO getInstance(){
+//        
+//        if(joinEventDAO == null){
+//            joinEventDAO = new JoinEventDAO() ; 
+//        }
+//        return joinEventDAO ; 
+//    }
     
     public JoinEvent retrieveJoinEvent (int userId, int eventId)
     {
         Criteria criteria = session.createCriteria(JoinEvent.class,"e")
-                .add(Restrictions.eq("e.users.id",userId ))
-                .add(Restrictions.eq("e.event.idEvent",eventId));
+                .add(Restrictions.eq("e.user.id",userId ))
+                .add(Restrictions.eq("e.event.id",eventId));
         
         List l =criteria.list();
         if(l.size()>0)
@@ -40,6 +58,15 @@ public class JoinEventDAO {
             return null;
         }
     }
+
+    public  List<JoinEvent> retrieveAttendedEvent(int id)
+    {
+        Criteria criteria = session.createCriteria(JoinEvent.class)
+                .add(Restrictions.eq("id.userId", id))
+                .add(Restrictions.eq("userStatue.id", 4));
+        List<JoinEvent> l = criteria.list();
+        return l;
+    }
     public boolean updateJoinEvent(JoinEvent jonEvent)
     {
         session.beginTransaction();
@@ -48,4 +75,32 @@ public class JoinEventDAO {
         return true;
     }
     
+    public boolean addJoinEvent(JoinEvent jonEvent)
+    {
+        try{
+            session.beginTransaction();
+            session.persist(jonEvent);  
+            session.getTransaction().commit();
+            return true;
+        }
+        catch(RuntimeException ex)
+        {
+            ex.printStackTrace();
+            return false;
+        }
+    }
+    public boolean deleteJoinEvent(JoinEvent jonEvent)
+    {
+        try{
+            session.beginTransaction();
+            session.delete(jonEvent);  
+            session.getTransaction().commit();
+            return true;
+        }
+        catch(Exception ex)
+        {
+            ex.printStackTrace();
+            return false;
+        }
+    }
 }
